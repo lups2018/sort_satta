2095dbfbb837705751adb783d4e89a83445c8592
Updesh Gupta <updesh.gupta@p360.com>
2023-07-16 05:16:13 -0400

    last changes

--- index.js
+++ index.js
@@ -3,16 +3,77 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
 var path = require('path');
+var bodyParser = require('body-parser')
+const session = require('express-session');
 
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/login', async (req, res) => {
+    try {
+        return res.render('login.ejs');
+    }
+    catch (err) {
+        console.log("err", err);
+    }
+});
 
-app.get('/', async (req, res) => {
+app.post('/login', async (req, res) => {
+    try {
+        let loginData = req.body
+        console.log("🚀 ~ file: index.js:26 ~ app.post ~ loginData:", loginData.username, loginData, process.env.EMAIL, process.env.PASSWORD);
+        if (process.env.EMAIL === loginData.username) {
+            if (process.env.PASSWORD === loginData.password) {
+                req.session.loggedIn = true;
+                return res.redirect('/sort');
+            }
+        }
+        else {
+            return res.redirect('back');
+        }
+    }
+    catch (err) {
+        console.log("err", err);
+    }
+});
+
+app.get('/sort', async (req, res) => {
+    try {
+        if (req.session.loggedIn) {
+        return res.render('test.ejs');
+        }
+        else{
+            return res.redirect('/login');
+        }
+    }
+    catch (err) {
+        console.log("err", err);
+    }
+});
+
+app.get('/sortOne', async (req, res) => {
     try {
 
-        return res.render('test.ejs');
+        return res.render('testOne.ejs');
+
+    }
+    catch (err) {
+        console.log("err", err);
+    }
+});
+
+app.get('/logout', async (req, res) => {
+    try {
+        req.session.loggedIn = false;
+        return res.redirect('/login');
 
     }
     catch (err) {
